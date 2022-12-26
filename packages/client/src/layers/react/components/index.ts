601f35d21647691622cd92f0421bb99bf6569646
Pranav <pranavkp.me@outlook.com>
2022-12-26 17:54:54 +0530

    feat(react): login user details added

--- packages/client/src/layers/react/components/index.ts
+++ packages/client/src/layers/react/components/index.ts
@@ -2,10 +2,12 @@ import { registerComponentBrowser } from "./ComponentBrowser";
 import { registerActionQueue } from "./ActionQueue";
 import { registerLoadingState } from "./LoadingState";
 import { registerNameScreen } from "./NameEnter";
+import { registerPlayerDetails } from "./PlayerDetails";
 
 export function registerUIComponents() {
   registerNameScreen();
   registerComponentBrowser();
+  registerPlayerDetails();
   registerLoadingState();
   registerActionQueue();
 }
