f50596c24b3891c8f431cdfb19ee710f123d578d
Viraj Kokane <virajkokane123@gmail.com>
2022-12-31 15:09:39 +0530

    feat(contracts): added upgrade system

---
+++ packages/contracts/src/systems/UpgradeSystem.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import "solecs/System.sol";
+import { IWorld } from "solecs/interfaces/IWorld.sol";
+import { getAddressById, addressToEntity } from "solecs/utils.sol";
+import { CashComponent, ID as CashComponentID } from "../components/CashComponent.sol";
+import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../components/LastUpdatedTimeComponent.sol";
+import { OwnedByComponent, ID as OwnedByComponentID } from "../components/OwnedByComponent.sol";
+import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
+import { getPlayerCash } from "../utils.sol";
+import { actionDelayInSeconds } from "../constants.sol";
+
+uint256 constant ID = uint256(keccak256("system.Upgrade"));
+
+contract UpgradeSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function execute(bytes memory arguments) public returns (bytes memory) {
+    uint256 entity = abi.decode(arguments, (uint256));
+
+    require(
+      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(entity) == addressToEntity(msg.sender),
+      "Entity not owned by user"
+    );
+
+    uint256 playerLastUpdatedTime = LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID))
+      .getValue(addressToEntity(msg.sender));
+
+    require(
+      playerLastUpdatedTime > 0 && block.timestamp >= playerLastUpdatedTime + actionDelayInSeconds,
+      "Need 10 seconds of delay between actions"
+    );
+
+    uint256 selectedEntityLevel = LevelComponent(getAddressById(components, LevelComponentID)).getValue(entity);
+
+    require(selectedEntityLevel >= 1, "Invalid entity");
+
+    require(selectedEntityLevel == 10, "Maximum level reached");
+
+    uint256 nextLevel = selectedEntityLevel + 1;
+
+    uint256 upgradeCost = (nextLevel**2) * 1000;
+
+    uint256 playerCash = getPlayerCash(
+      CashComponent(getAddressById(components, CashComponentID)),
+      addressToEntity(msg.sender)
+    );
+
+    require(playerCash >= upgradeCost, "Not enough money to upgrade");
+
+    CashComponent(getAddressById(components, CashComponentID)).set(
+      addressToEntity(msg.sender),
+      playerCash - upgradeCost
+    );
+    LevelComponent(getAddressById(components, LevelComponentID)).set(entity, nextLevel);
+    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(
+      addressToEntity(msg.sender),
+      block.timestamp
+    );
+    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(entity, block.timestamp);
+  }
+
+  function executeTyped(uint256 entity) public returns (bytes memory) {
+    return execute(abi.encode(entity));
+  }
+}
