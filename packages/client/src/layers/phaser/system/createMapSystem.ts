86f081a954c23c10438cb020f8ca82f81f518453
twinkal <twinkal.work@outlook.com>
2023-01-05 20:27:08 +0530

    feat: orbit comor and size fixed

--- packages/client/src/layers/phaser/system/createMapSystem.ts
+++ packages/client/src/layers/phaser/system/createMapSystem.ts
@@ -22,43 +22,53 @@ export function createMapSystem(network: NetworkLayer, phaser: PhaserLayer) {
   const { x, y } = tileCoordToPixelCoord({ x: -1, y: -1 }, tileWidth, tileHeight);
 
   const circle1 = phaserScene.add.circle(32, 32)
-  const label1 = phaserScene.add.text(32,-128, "5", {fontSize: "24px", color: "#ffffff"})
+  const label1 = phaserScene.add.text(32,-128, "5", {fontSize: "24px", color: "#D3D3D3"})
   const circle2 = phaserScene.add.circle(32, 32)
-  const label2 = phaserScene.add.text(32,-288, "10", {fontSize: "24px", color: "#ffffff"})
+  const label2 = phaserScene.add.text(32,-288, "10", {fontSize: "24px", color: "#D3D3D3"})
   const circle3 = phaserScene.add.circle(32, 32)
-  const label3 = phaserScene.add.text(32,-448, "15", {fontSize: "24px", color: "#ffffff"})
+  const label3 = phaserScene.add.text(32,-448, "15", {fontSize: "24px", color: "#D3D3D3"})
   const circle4  = phaserScene.add.circle( 32,32)
-  const label4 = phaserScene.add.text(32,-608, "20", {fontSize: "24px", color: "#ffffff"})
+  const label4 = phaserScene.add.text(32,-608, "20", {fontSize: "24px", color: "#D3D3D3"})
   const circle5  = phaserScene.add.circle( 32,32)
-  const label5 = phaserScene.add.text(32,-768, "25", {fontSize: "24px", color: "#ffffff"})
+  const label5 = phaserScene.add.text(32,-768, "25", {fontSize: "24px", color: "#D3D3D3"})
   const circle6  = phaserScene.add.circle( 32,32)
-  const label6 = phaserScene.add.text(32,-928, "30", {fontSize: "24px", color: "#ffffff"})
+  const label6 = phaserScene.add.text(32,-928, "30", {fontSize: "24px", color: "#D3D3D3"})
 
 
-  circle1.setStrokeStyle(0.05, 0xffffff, 1)
+  circle1.setStrokeStyle(1, 0xD3D3D3 )
   circle1.setDisplaySize(320, 320)
-  circle1.setDepth(10)
-  label1.setOrigin(0.5, 0.5)
-  circle2.setStrokeStyle(0.05, 0xffffff, 1)
+  label1.setOrigin(1, 0.5)
+  label1.setDepth(20)
+
+  circle2.setStrokeStyle(0.6, 0xD3D3D3)
   circle2.setDisplaySize(640, 640)
-  circle2.setDepth(10)
   label2.setOrigin(0.5, 0.5)
-  circle3.setStrokeStyle(0.05, 0xffffff, 1)
+  label2.setDepth(20)
+
+
+  circle3.setStrokeStyle(0.5, 0xD3D3D3)
   circle3.setDisplaySize(960, 960)
-  circle3.setDepth(10)
   label3.setOrigin(0.5, 0.5)
-  circle4.setStrokeStyle(0.05, 0xffffff, 1)
+  label3.setDepth(20)
+
+
+  circle4.setStrokeStyle(0.4, 0xD3D3D3)
   circle4.setDisplaySize(1280, 1280)
-  circle4.setDepth(10)
   label4.setOrigin(0.5, 0.5)
-  circle5.setStrokeStyle(0.05, 0xffffff, 1)
+  label4.setDepth(20)
+
+
+  circle5.setStrokeStyle(0.3, 0xD3D3D3)
   circle5.setDisplaySize(1600, 1600)
-  circle5.setDepth(10)
   label5.setOrigin(0.5, 0.5)
-  circle6.setStrokeStyle(0.05, 0xffffff, 1)
+  label5.setDepth(20)
+
+
+  circle6.setStrokeStyle(0.2, 0xD3D3D3)
   circle6.setDisplaySize(1920, 1920)
-  circle6.setDepth(10)
   label6.setOrigin(0.5, 0.5)
+  label6.setDepth(20)
+
 
   const centerSun = config.assets[Assets.Center];
   object.setComponent({
