27e9e12de0a2515faa3f9b4ecc4e3baaac08cf44
Viraj Kokane <virajkokane123@gmail.com>
2023-01-17 14:07:04 +0530

    fix(phaser): fixed zoom for trackpad and mouse

--- packages/client/src/layers/phaser/system/createMapSystem.ts
+++ packages/client/src/layers/phaser/system/createMapSystem.ts
@@ -120,16 +120,18 @@ export function createMapSystem(network: NetworkLayer, phaser: PhaserLayer) {
       );
     }
   });
-  // if (phaserScene.input.activePointer.pointerId === -1) {
-  //   phaserScene.input.on("wheel", (event: { deltaY: number }) => {
-  //     let zoom = camera.phaserCamera.zoom;
-  //     if (event.deltaY > 0) {
-  //       zoom -= 0.1;
-  //     } else {
-  //       zoom += 0.1;
-  //     }
-  //     camera.phaserCamera.setZoom(zoom);
-  //   });
-  // }
+
+  phaserScene.input.on("wheel", (event: { deltaY: number }) => {
+    if (Math.abs(event.deltaY) > 300) {
+      let zoom = camera.phaserCamera.zoom;
+      if (event.deltaY > 0) {
+        zoom -= 0.1;
+      } else {
+        zoom += 0.1;
+      }
+      camera.phaserCamera.setZoom(zoom);
+    }
+  });
+
   camera.phaserCamera.setBounds(-2000, -2000, 4200, 4200, true);
 }
