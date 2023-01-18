085b3f9adb8dbc4e4b2c41b111d6a48f765d62c3
Pranav <pranavkp.me@outlook.com>
2023-01-18 15:33:01 +0530

    fix(attack log): fix attack log

--- packages/client/src/layers/local/stream-system/system.Attack.ts
+++ packages/client/src/layers/local/stream-system/system.Attack.ts
@@ -12,31 +12,43 @@ export function systemAttack(network: NetworkLayer, phaser: PhaserLayer) {
   const {
     localApi: { setLogs },
   } = phaser;
-  defineRxSystem(world, systemCallStreams["system.Attack"], ({ args }) => {
-    console.log(args);
+  defineRxSystem(world, systemCallStreams["system.Attack"], ({ args, updates }) => {
+    console.log(updates);
+
     const { destinationGodownEntity, sourceGodownEntity, amount } = args as {
       destinationGodownEntity: BigNumber;
       sourceGodownEntity: BigNumber;
       amount: BigNumber;
     };
-    const destinationGodownEntityIndex = world.entities.findIndex(
-      (entity) => entity === destinationGodownEntity._hex
-    ) as EntityIndex;
     const sourceGodownEntityIndex = world.entities.findIndex(
       (entity) => entity === sourceGodownEntity._hex
     ) as EntityIndex;
-    const destPosition = getComponentValue(Position, destinationGodownEntityIndex);
+    const destinationGodownEntityIndex = world.entities.findIndex(
+      (entity) => entity === destinationGodownEntity._hex
+    ) as EntityIndex;
     const srcPosition = getComponentValue(Position, sourceGodownEntityIndex);
+    const destPosition = getComponentValue(Position, destinationGodownEntityIndex);
+
     const srcOwnedBy = getComponentValue(OwnedBy, sourceGodownEntityIndex)?.value;
     const destOwnedBy = getComponentValue(OwnedBy, destinationGodownEntityIndex)?.value;
-    const destOwnedByIndex = world.entities.findIndex((entity) => entity === destOwnedBy) as EntityIndex;
+
     const srcOwnedByIndex = world.entities.findIndex((entity) => entity === srcOwnedBy) as EntityIndex;
+    const destOwnedByIndex = world.entities.findIndex((entity) => entity === destOwnedBy) as EntityIndex;
+
     const srcName = getComponentValue(Name, srcOwnedByIndex)?.value;
     const destName = getComponentValue(Name, destOwnedByIndex)?.value;
-    setLogs(
-      `${srcName} station at ${srcPosition?.x},${srcPosition?.y} attacked ${destName} station at ${destPosition?.x},${
-        destPosition?.y
-      }using  ${BigNumber.from(amount)} missiles `
-    );
+    if (destName) {
+      setLogs(
+        `${srcName} station at ${srcPosition?.x},${srcPosition?.y} attacked ${destName} station at ${destPosition?.x},${
+          destPosition?.y
+        } using  ${BigNumber.from(amount)} missiles `
+      );
+    } else {
+      setLogs(
+        `${srcName} station at ${srcPosition?.x},${srcPosition?.y} attacked ${destName} station at ${destPosition?.x},${
+          destPosition?.y
+        } using  ${BigNumber.from(amount)} missiles and destroyed it`
+      );
+    }
   });
 }
