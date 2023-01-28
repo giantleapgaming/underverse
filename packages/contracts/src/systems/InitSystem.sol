ce92109c017af4e27c3d7738885c52629bd082cd
sanket-kogekar <kogekar@proton.me>
2023-01-28 05:32:39 +0530

    feat: asteroids

--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -7,12 +7,18 @@ import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../c
 import { NameComponent, ID as NameComponentID } from "../components/NameComponent.sol";
 import { CashComponent, ID as CashComponentID } from "../components/CashComponent.sol";
 import { playerInitialCash } from "../constants.sol";
+import { PositionComponent, ID as PositionComponentID, Coord } from "../components/PositionComponent.sol";
 
 uint256 constant ID = uint256(keccak256("system.Init"));
 
 contract InitSystem is System {
   mapping(address => bool) registeredPlayers;
   uint256 private playerCount;
+  struct AsteroidCoords {
+    int32 x;
+    int32 y;
+  }
+  AsteroidCoords[] private myAsteroidCoordsArray;
 
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
@@ -31,11 +37,49 @@ contract InitSystem is System {
 
     NameComponent(getAddressById(components, NameComponentID)).set(addressToEntity(msg.sender), name);
 
+    // Init called for first time.
+    if (playerCount == 0) {
+      ////////////////////////
+      ////////////////////////
+      // Generate 18 random points - Below logic works well.
+      uint32 count = 0;
+      int256 failedTries = 0;
+      while (count < 18) {
+        uint256 seed = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, count, failedTries)));
+        int32 x = int32(int256(seed) % 41) - 25;
+        if (x < 0) {
+          x += 40;
+        }
+
+        uint256 seed2 = uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, failedTries, count)));
+        int32 y = int32((int256(seed2) + int256(x)) % 41) - 25;
+        if (y < 0) {
+          y += 40;
+        }
+
+        // Ensure that the coordinates are within the specified range
+        if (((x <= -15 && x >= -25) || (x >= 15 && x <= 25)) && ((y <= -15 && y >= -25) || (y >= 15 && y <= 25))) {
+          AsteroidCoords memory myAsteroidCoords = AsteroidCoords({ x: x, y: y });
+          myAsteroidCoordsArray.push(myAsteroidCoords);
+          count++;
+          failedTries--;
+        } else {
+          failedTries++;
+        }
+      }
+      ////////////////////////
+      ////////////////////////
+    }
     registeredPlayers[msg.sender] = true;
     playerCount += 1;
   }
 
   function executeTyped(string calldata name) public returns (bytes memory) {
     return execute(abi.encode(name));
+  }
+
+  // Temporary function to check if storage array elements are truly random
+  function getAsteroidCoordsArray() public view returns (AsteroidCoords[] memory) {
+    return myAsteroidCoordsArray;
   }
 }
