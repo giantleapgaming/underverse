b1e271e0218a8457be98ababdbd1a2c7a134a409
Viraj Kokane <virajkokane123@gmail.com>
2023-01-29 10:26:28 +0530

    fix(contracts): fixed id of faction component

--- packages/contracts/src/components/FactionComponent.sol
+++ packages/contracts/src/components/FactionComponent.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.8.0;
 import "std-contracts/components/Uint256Component.sol";
 
-uint256 constant ID = uint256(keccak256("component.Level"));
+uint256 constant ID = uint256(keccak256("component.Faction"));
 
 contract FactionComponent is Uint256Component {
   constructor(address world) Uint256Component(world, ID) {}
