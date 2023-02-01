b7bebbc90d8f29e6991e185b411efbf90e038dde
Viraj Kokane <virajkokane@Virajs-MacBook-Pro.local>
2023-02-01 15:19:18 +0530

    fix(build system): fixed entity type in build system

--- packages/contracts/src/systems/BuildSystem.sol
+++ packages/contracts/src/systems/BuildSystem.sol
@@ -14,7 +14,7 @@ import { BalanceComponent, ID as BalanceComponentID } from "../components/Balanc
 //Moresh
 import { EntityTypeComponent, ID as EntityTypeComponentID } from "../components/EntityTypeComponent.sol";
 import { getCurrentPosition, getPlayerCash, getLastUpdatedTimeOfEntity, getGodownCreationCost } from "../utils.sol";
-import { actionDelayInSeconds, offenceInitialAmount, defenceInitialAmount, godownInitialLevel, godownInitialStorage, godownInitialBalance, MULTIPLIER, MULTIPLIER2, godownType } from "../constants.sol";
+import { actionDelayInSeconds, offenceInitialAmount, defenceInitialAmount, godownInitialLevel, godownInitialStorage, godownInitialBalance, MULTIPLIER, MULTIPLIER2 } from "../constants.sol";
 import "../libraries/Math.sol";
 
 uint256 constant ID = uint256(keccak256("system.Build"));
@@ -127,7 +127,7 @@ contract BuildSystem is System {
     OffenceComponent(getAddressById(components, OffenceComponentID)).set(godownEntity, offenceInitialAmount);
     DefenceComponent(getAddressById(components, DefenceComponentID)).set(godownEntity, defenceInitialAmount);
     LevelComponent(getAddressById(components, LevelComponentID)).set(godownEntity, godownInitialLevel);
-    EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).set(godownEntity, godownType);
+    EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).set(godownEntity, entity_type);
     // StorageComponent(getAddressById(components, StorageComponentID)).set(godownEntity, godownInitialStorage);
     BalanceComponent(getAddressById(components, BalanceComponentID)).set(godownEntity, godownInitialBalance);
     //Moresh: Assign Entity type
