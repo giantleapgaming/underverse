4ecbacdf80c9cc85766e0872a72acb3e17af2218
moreshk <moreshk@gmail.com>
2023-02-06 17:14:09 +1100

    feat(moveship): changed use of cash to fuel

--- packages/contracts/src/systems/MoveShipSystem.sol
+++ packages/contracts/src/systems/MoveShipSystem.sol
@@ -1,11 +1,11 @@
 //Move: Allows the movement of Harvester and Attack entity types from current position to a new position
-
+//We will be using Fuel as the resource to move rather than Cash
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import "solecs/System.sol";
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById, addressToEntity } from "solecs/utils.sol";
-import { CashComponent, ID as CashComponentID } from "../components/CashComponent.sol";
+import { FuelComponent, ID as FuelComponentID } from "../components/FuelComponent.sol";
 import { PositionComponent, ID as PositionComponentID, Coord } from "../components/PositionComponent.sol";
 import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../components/LastUpdatedTimeComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "../components/OwnedByComponent.sol";
@@ -13,7 +13,7 @@ import { BalanceComponent, ID as BalanceComponentID } from "../components/Balanc
 import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
 //Importing Entity Type
 import { EntityTypeComponent, ID as EntityTypeComponentID } from "../components/EntityTypeComponent.sol";
-import { getCurrentPosition, getPlayerCash, getLastUpdatedTimeOfEntity, getEntityLevel, getDistanceBetweenCoordinatesWithMultiplier, getCoords, findEnclosedPoints, checkIntersections } from "../utils.sol";
+import { getCurrentPosition, getLastUpdatedTimeOfEntity, getEntityLevel, getDistanceBetweenCoordinatesWithMultiplier, getCoords, findEnclosedPoints, checkIntersections } from "../utils.sol";
 import { MULTIPLIER, MULTIPLIER2 } from "../constants.sol";
 import "../libraries/Math.sol";
 
@@ -88,17 +88,18 @@ contract MoveShipSystem is System {
     //Transport cost is a square function of the distance and level of the entity 
     uint256 totalTransportCost = ((distanceBetweenGodowns * sourceEntityLevel)**2);
 
-    uint256 playerCash = getPlayerCash(
-      CashComponent(getAddressById(components, CashComponentID)),
-      addressToEntity(msg.sender)
+
+    uint256 playerFuel = FuelComponent(getAddressById(components, FuelComponentID)).getValue(
+      sourceEntity
     );
 
-    require(playerCash >= totalTransportCost, "Not enough money to transport product");
+
+    require(playerFuel >= totalTransportCost, "Not enough fuel to transport product");
 
     // update player data
-    CashComponent(getAddressById(components, CashComponentID)).set(
+    FuelComponent(getAddressById(components, FuelComponentID)).set(
       addressToEntity(msg.sender),
-      playerCash - totalTransportCost
+      playerFuel - totalTransportCost
     );
 
     PositionComponent(getAddressById(components, PositionComponentID)).set(
