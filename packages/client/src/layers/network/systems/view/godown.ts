5484aa55da9eab475f1032909c69831e450550c2
Pranav <pranavkp.me@outlook.com>
2023-02-07 23:05:13 +0530

    fix(client): health bar added on godown and harvest

--- packages/client/src/layers/network/systems/view/godown.ts
+++ packages/client/src/layers/network/systems/view/godown.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { getComponentValue, getComponentValueStrict } from "@latticexyz/recs";
 import { tileCoordToPixelCoord } from "@latticexyz/phaserx";
 import { defineSystem, Has } from "@latticexyz/recs";
@@ -5,6 +6,7 @@ import { NetworkLayer } from "../../../network";
 import { PhaserLayer } from "../../../phaser";
 import { Sprites } from "../../../phaser/constants";
 import { Mapping } from "../../../../utils/mapping";
+import { factionData } from "../../../../utils/constants";
 
 export function displayGodownSystem(network: NetworkLayer, phaser: PhaserLayer) {
   const {
@@ -12,6 +14,7 @@ export function displayGodownSystem(network: NetworkLayer, phaser: PhaserLayer) 
     scenes: {
       Main: {
         objectPool,
+        phaserScene,
         config,
         maps: {
           Main: { tileWidth, tileHeight },
@@ -26,6 +29,30 @@ export function displayGodownSystem(network: NetworkLayer, phaser: PhaserLayer) 
     world,
     [Has(Position), Has(Balance), Has(EntityType), Has(Level), Has(OwnedBy), Has(Defence)],
     ({ entity }) => {
+      const healthBg = phaserScene.children
+        .getChildren()
+        // @ts-ignore
+        .find((item) => item.id === `godown-health-bar-bg-${entity}`)
+        // @ts-ignore
+        ?.clear();
+      const health = phaserScene.children
+        .getChildren()
+        // @ts-ignore
+        .find((item) => item.id === `godown-health-bar-bg-${entity}`)
+        // @ts-ignore
+        ?.clear();
+      const healthBar = health ?? phaserScene.add.graphics();
+      const healthBarBg = healthBg ?? phaserScene.add.graphics();
+      !healthBg &&
+        Object.defineProperty(healthBar, "id", {
+          value: `godown-health-bar-bg-${entity}`,
+          writable: true,
+        });
+      !health &&
+        Object.defineProperty(healthBarBg, "id", {
+          value: `godown-health-bar-${entity}`,
+          writable: true,
+        });
       const entityTypeNumber = getComponentValue(EntityType, entity)?.value;
       if (entityTypeNumber && +entityTypeNumber === Mapping.godown.id) {
         const ownedBy = getComponentValueStrict(OwnedBy, entity).value;
@@ -37,6 +64,18 @@ export function displayGodownSystem(network: NetworkLayer, phaser: PhaserLayer) 
         const defence = getComponentValueStrict(Defence, entity).value;
         const { x, y } = tileCoordToPixelCoord({ x: position.x, y: position.y }, tileWidth, tileHeight);
         if (+defence > 0 && faction && typeof +faction === "number") {
+          const progress = +defence / (+level * 100);
+          const endAngle = Phaser.Math.DegToRad(360 * progress);
+          healthBarBg.lineStyle(6, 0xd3d3d3, 1);
+          healthBarBg.arc(x + 32, y + 32, 45, Phaser.Math.DegToRad(0), 360);
+          healthBarBg.setAlpha(0.1);
+          healthBarBg.setDepth(99);
+          healthBarBg.strokePath();
+          healthBar.setAlpha(0.4);
+          healthBar.lineStyle(6, +`0x${factionData[+faction - 1].color.split("#")[1]}`, 1);
+          healthBar.arc(x + 32, y + 32, 45, Phaser.Math.DegToRad(0), endAngle);
+          healthBar.strokePath();
+          healthBar.setDepth(100);
           const astroidObject = objectPool.get(`godown-${entity}`, "Sprite");
           const factionObject = objectPool.get(`godown-faction-${entity}`, "Sprite");
           const godown = config.sprites[Sprites.Asteroid12];
@@ -61,6 +100,8 @@ export function displayGodownSystem(network: NetworkLayer, phaser: PhaserLayer) 
         } else {
           objectPool.remove(`godown-${entity}`);
           objectPool.remove(`godown-faction-${entity}`);
+          health?.clear();
+          healthBg?.clear();
         }
       }
     }
--- packages/client/src/layers/network/systems/view/harvester.ts
+++ packages/client/src/layers/network/systems/view/harvester.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { getComponentValue, getComponentValueStrict } from "@latticexyz/recs";
 import { tileCoordToPixelCoord } from "@latticexyz/phaserx";
 import { defineSystem, Has } from "@latticexyz/recs";
@@ -5,6 +6,7 @@ import { NetworkLayer } from "../../../network";
 import { PhaserLayer } from "../../../phaser";
 import { Sprites } from "../../../phaser/constants";
 import { Mapping } from "../../../../utils/mapping";
+import { factionData } from "../../../../utils/constants";
 
 export function displayHarvesterSystem(network: NetworkLayer, phaser: PhaserLayer) {
   const {
@@ -12,6 +14,7 @@ export function displayHarvesterSystem(network: NetworkLayer, phaser: PhaserLaye
     scenes: {
       Main: {
         objectPool,
+        phaserScene,
         config,
         maps: {
           Main: { tileWidth, tileHeight },
@@ -26,6 +29,30 @@ export function displayHarvesterSystem(network: NetworkLayer, phaser: PhaserLaye
     world,
     [Has(Position), Has(Balance), Has(EntityType), Has(Level), Has(OwnedBy), Has(Defence)],
     ({ entity }) => {
+      const healthBg = phaserScene.children
+        .getChildren()
+        // @ts-ignore
+        .find((item) => item.id === `harvester-health-bar-bg-${entity}`)
+        // @ts-ignore
+        ?.clear();
+      const health = phaserScene.children
+        .getChildren()
+        // @ts-ignore
+        .find((item) => item.id === `harvester-health-bar-bg-${entity}`)
+        // @ts-ignore
+        ?.clear();
+      const healthBar = health ?? phaserScene.add.graphics();
+      const healthBarBg = healthBg ?? phaserScene.add.graphics();
+      !healthBg &&
+        Object.defineProperty(healthBar, "id", {
+          value: `harvester-health-bar-bg-${entity}`,
+          writable: true,
+        });
+      !health &&
+        Object.defineProperty(healthBarBg, "id", {
+          value: `harvester-health-bar-${entity}`,
+          writable: true,
+        });
       const entityTypeNumber = getComponentValue(EntityType, entity)?.value;
       if (entityTypeNumber && +entityTypeNumber === Mapping.harvester.id) {
         const ownedBy = getComponentValueStrict(OwnedBy, entity).value;
@@ -37,6 +64,18 @@ export function displayHarvesterSystem(network: NetworkLayer, phaser: PhaserLaye
         const defence = getComponentValueStrict(Defence, entity).value;
         const { x, y } = tileCoordToPixelCoord({ x: position.x, y: position.y }, tileWidth, tileHeight);
         if (+defence > 0 && faction && typeof +faction === "number") {
+          const progress = +defence / (+level * 100);
+          const endAngle = Phaser.Math.DegToRad(360 * progress);
+          healthBarBg.lineStyle(6, 0xd3d3d3, 1);
+          healthBarBg.arc(x + 32, y + 32, 45, Phaser.Math.DegToRad(0), 360);
+          healthBarBg.setAlpha(0.1);
+          healthBarBg.setDepth(99);
+          healthBarBg.strokePath();
+          healthBar.setAlpha(0.4);
+          healthBar.lineStyle(6, +`0x${factionData[+faction - 1].color.split("#")[1]}`, 1);
+          healthBar.arc(x + 32, y + 32, 45, Phaser.Math.DegToRad(0), endAngle);
+          healthBar.strokePath();
+          healthBar.setDepth(100);
           const astroidObject = objectPool.get(`harvester-${entity}`, "Sprite");
           const factionObject = objectPool.get(`harvester-faction-${entity}`, "Sprite");
           const harvester = config.sprites[Sprites.Asteroid12];
@@ -60,6 +99,8 @@ export function displayHarvesterSystem(network: NetworkLayer, phaser: PhaserLaye
         } else {
           objectPool.remove(`harvester-${entity}`);
           objectPool.remove(`harvester-faction-${entity}`);
+          health?.clear();
+          healthBg?.clear();
         }
       }
     }
