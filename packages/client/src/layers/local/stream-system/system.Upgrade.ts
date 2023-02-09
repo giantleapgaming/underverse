11c9b272bb9811967f9a60277073d25122ec5aa5
Pranav <pranavkp.me@outlook.com>
2023-02-09 12:26:13 +0530

    feat(client): log for upgarde system added

--- packages/client/src/layers/local/stream-system/system.Upgrade.ts
+++ packages/client/src/layers/local/stream-system/system.Upgrade.ts
@@ -1,14 +1,16 @@
-import { defineRxSystem, EntityIndex, getComponentEntities, getComponentValue } from "@latticexyz/recs";
+import { numberMapping } from "./../../../utils/mapping";
+import { defineRxSystem, EntityIndex, getComponentValue } from "@latticexyz/recs";
 import { BigNumber } from "ethers";
+import { factionData } from "../../../utils/constants";
 import { NetworkLayer } from "../../network";
 import { PhaserLayer } from "../../phaser";
-import { playersColor } from "./system.Buy";
+import { colorString } from "./utils";
 
 export function systemUpgrade(network: NetworkLayer, phaser: PhaserLayer) {
   const {
     world,
     systemCallStreams,
-    components: { OwnedBy, Position, Name, Level },
+    components: { OwnedBy, Position, Name, Level, Faction, EntityType },
   } = network;
   const {
     localApi: { setLogs },
@@ -17,17 +19,30 @@ export function systemUpgrade(network: NetworkLayer, phaser: PhaserLayer) {
     const { entity } = args as { entity: BigNumber };
     const godownEntityIndex = world.entities.findIndex((worldEntity) => worldEntity === entity._hex) as EntityIndex;
     const position = getComponentValue(Position, godownEntityIndex);
+    const entityType = getComponentValue(EntityType, godownEntityIndex)?.value;
     const level = getComponentValue(Level, godownEntityIndex)?.value;
     const ownedBy = getComponentValue(OwnedBy, godownEntityIndex)?.value;
     const ownedByIndex = world.entities.findIndex((entity) => entity === ownedBy) as EntityIndex;
     const name = getComponentValue(Name, ownedByIndex)?.value;
-
-    const allUserNameEntityId = [...getComponentEntities(Name)];
-    const userIndex = allUserNameEntityId.indexOf(ownedByIndex) as EntityIndex;
-    setLogs(
-      `<p><span style="color:${playersColor[userIndex]};font-weight:bold">${name}</span> upgraded station (${
-        position?.x
-      },${position?.y}) to ${BigNumber.from(level)}`
-    );
+    const faction = getComponentValue(Faction, ownedByIndex)?.value;
+    if (
+      faction &&
+      entityType &&
+      level &&
+      typeof +faction === "number" &&
+      typeof +entityType === "number" &&
+      typeof +level === "number"
+    ) {
+      const color = factionData[+faction - 1]?.color;
+      const stationName = numberMapping[+entityType].name;
+      setLogs(
+        `<p>${colorString({ name, color })} upgraded ${colorString({ name: stationName, color })} station at (${
+          position?.x
+        },${position?.y}) from level ${colorString({ name: `${+level - 1}`, color: "#ffffff" })} to ${colorString({
+          name: `${+level}`,
+          color,
+        })}`
+      );
+    }
   });
 }
