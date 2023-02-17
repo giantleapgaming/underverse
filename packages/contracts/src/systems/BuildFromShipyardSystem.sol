9a2ae8a41040d129a425e6818bdace2debcb51a0
moreshk <moreshk@gmail.com>
2023-02-17 16:04:34 +1100

    feat(build from shipyard): new system to allow building from Shipyard
    Users can now build attack ships and harvesters from a shipyard

--- packages/contracts/src/systems/BuildFromShipyardSystem.sol
+++ packages/contracts/src/systems/BuildFromShipyardSystem.sol
@@ -1,27 +1,19 @@
-//Designed as a replacement for the legacy build system
-//Allows us to move from a board game style design to a physicalized resources style RTS
-//We will get rid of the use of cash and replace it with actual resources
-//You need some metal to build and fuel to transport the material to the destination where you want it built
-//Also some additional fuel is needed to fillup the tanks of the new vessel that is being built so that it has some fueld to start
-//This system can only be initiated from ship yards
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import "solecs/System.sol";
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById, addressToEntity } from "solecs/utils.sol";
+import { FuelComponent, ID as FuelComponentID } from "../components/FuelComponent.sol";
 import { PositionComponent, ID as PositionComponentID, Coord } from "../components/PositionComponent.sol";
 import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../components/LastUpdatedTimeComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "../components/OwnedByComponent.sol";
 import { BalanceComponent, ID as BalanceComponentID } from "../components/BalanceComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
-//Importing Entity Type
+import { atleastOneObstacleOnTheWay, isThereAnyObstacleOnTheWay, getCurrentPosition, getPlayerFuel, getLastUpdatedTimeOfEntity, getEntityLevel, getDistanceBetweenCoordinatesWithMultiplier, getFactionTransportCosts } from "../utils.sol";
 import { EntityTypeComponent, ID as EntityTypeComponentID } from "../components/EntityTypeComponent.sol";
-import { FuelComponent, ID as FuelComponentID } from "../components/FuelComponent.sol";
-import { OffenceComponent, ID as OffenceComponentID } from "../components/OffenceComponent.sol";
 import { DefenceComponent, ID as DefenceComponentID } from "../components/DefenceComponent.sol";
-import { getCurrentPosition, getPlayerCash, getLastUpdatedTimeOfEntity, getEntityLevel, getDistanceBetweenCoordinatesWithMultiplier, getCoords, findEnclosedPoints, checkIntersections, getPlayerFuel } from "../utils.sol";
-import { MULTIPLIER, MULTIPLIER2, offenceInitialAmount, defenceInitialAmount, godownInitialLevel, godownInitialBalance, godownInitialFuel } from "../constants.sol";
+import { PopulationComponent, ID as PopulationComponentID } from "../components/PopulationComponent.sol";
+import { actionDelayInSeconds, defenceInitialAmount, godownInitialLevel, godownInitialBalance, initialEntityPopulation, baseInitialfuel } from "../constants.sol";
 import "../libraries/Math.sol";
 
 uint256 constant ID = uint256(keccak256("system.BuildFromShipyard"));
@@ -30,111 +22,86 @@ contract BuildFromShipyardSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function execute(bytes memory arguments) public returns (bytes memory) {
-    (uint256 sourceEntity, int32 x, int32 y, uint256 entity_type) = abi.decode(
+    (uint256 ShipyardEntity, int32 x, int32 y, uint256 entity_type) = abi.decode(
       arguments,
       (uint256, int32, int32, uint256)
     );
 
-    // Check if the source entity is owned by the user
     require(
-      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(sourceEntity) == addressToEntity(msg.sender), "Ship not owned by user");
+      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(ShipyardEntity) ==
+        addressToEntity(msg.sender),
+      "Shipyard not owned by user"
+    );
 
-    //Check that the ship is not a destroyed ship (Level 0)
+    require(
+      LevelComponent(getAddressById(components, LevelComponentID)).getValue(ShipyardEntity) >= 1,
+      "Invalid Shipyard entity"
+    );
 
-    require(LevelComponent(getAddressById(components, LevelComponentID)).getValue(
-      sourceEntity
-    ) >= 1, "Ship has already been destroyed");
+    require(
+      EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).getValue(ShipyardEntity) == 7,
+      "Source entity has to be a Shipyard"
+    );
 
-    //Check that the entity is a shipyard
- 
-    require((EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).getValue(
-      sourceEntity
-    ) == 7), "Only Shipyards can initiate build");
+    require((entity_type == 4 || entity_type == 5), "Can only build Attack or Harvester ships");
 
+    uint256 balanceShipyard = BalanceComponent(getAddressById(components, BalanceComponentID)).getValue(ShipyardEntity);
 
-    Coord memory sourcePosition = getCurrentPosition(
+    require(balanceShipyard >= 2, "Need atleast 2 minerals to build anything");
+
+    Coord memory ShipyardPosition = getCurrentPosition(
       PositionComponent(getAddressById(components, PositionComponentID)),
-      sourceEntity
+      ShipyardEntity
     );
 
-    //Destination position
-    Coord memory destinationPosition;
-    
-    destinationPosition.x = x;
-    destinationPosition.y = y;
+    Coord memory buildPosition = Coord({ x: x, y: y });
 
-    PositionComponent position = PositionComponent(getAddressById(components, PositionComponentID));
+    //Check that the build location and Shipyard are no further than 5 units from each other
 
-    uint256[] memory allPositionEntities = position.getEntities();
-
-    Coord[] memory allStationCoords = getCoords(allPositionEntities, components);
-
-    //Check if any stations are blocking path
-    //This will ensure you cannot move to a destination if there is something in the way or something at the destination as well
-
-    Coord[] memory blockingCoords = checkIntersections(
-      sourcePosition,
-      destinationPosition,
-      // enclosedPoints
-      allStationCoords
+    require(
+      getDistanceBetweenCoordinatesWithMultiplier(ShipyardPosition, buildPosition) <= 5000,
+      "Shipyard is further than 5 units distance from build location"
     );
 
-// 1 entity is always returned as the start and the end points, if there are 2+ returned then that means there is a blocker
-
-    require(blockingCoords.length <= 1, "Blocker entity in the way");
-
-    //END: Check if enemy stations are blocking path
-    
-    //Calculate cost of transport
-
-    uint256 distance = getDistanceBetweenCoordinatesWithMultiplier(
-      sourcePosition,
-      destinationPosition
+    require(
+      atleastOneObstacleOnTheWay(
+        ShipyardPosition.x,
+        ShipyardPosition.y,
+        buildPosition.x,
+        buildPosition.y,
+        components
+      ) == false,
+      "Obstacle on the way"
     );
 
-    //Check if you have enough material on the shipyard to build the station
-    require(BalanceComponent(getAddressById(components, BalanceComponentID)).getValue(
-      sourceEntity
-    ) > 0,"Not enough resources");
-
-    uint256 playerFuel = FuelComponent(getAddressById(components, FuelComponentID)).getValue(
-      sourceEntity
-    );
-
-    //Transport fuel cost is a square function of the distance plus some reserves for the new station
-    uint256 totalTransportCost = ((distance)**2) + godownInitialFuel;
-
-    require(playerFuel >= totalTransportCost, "Not enough fuel to transport material and fuel up new station");
-
-    uint256 newEntityID = world.getUniqueEntityId();
-
-    PositionComponent(getAddressById(components, PositionComponentID)).set(newEntityID, destinationPosition);
-    OwnedByComponent(getAddressById(components, OwnedByComponentID)).set(newEntityID, addressToEntity(msg.sender));
-    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(newEntityID, block.timestamp);
-    OffenceComponent(getAddressById(components, OffenceComponentID)).set(newEntityID, offenceInitialAmount);
-    DefenceComponent(getAddressById(components, DefenceComponentID)).set(newEntityID, defenceInitialAmount);
-    LevelComponent(getAddressById(components, LevelComponentID)).set(newEntityID, godownInitialLevel);
-    EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).set(newEntityID, entity_type);
-    BalanceComponent(getAddressById(components, BalanceComponentID)).set(newEntityID, godownInitialBalance);
-    //Moresh: Assign Entity type
-
-    // update player data
-    FuelComponent(getAddressById(components, FuelComponentID)).set(sourceEntity, playerFuel - totalTransportCost);
-    FuelComponent(getAddressById(components, FuelComponentID)).set(newEntityID, godownInitialFuel);
     LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(
       addressToEntity(msg.sender),
       block.timestamp
     );
 
+    BalanceComponent(getAddressById(components, BalanceComponentID)).set(ShipyardEntity, balanceShipyard - 2);
+
+    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(
+      ShipyardEntity,
+      block.timestamp
+    );
+
+    uint256 buildEntity = world.getUniqueEntityId();
+
+    PositionComponent(getAddressById(components, PositionComponentID)).set(buildEntity, buildPosition);
+    OwnedByComponent(getAddressById(components, OwnedByComponentID)).set(buildEntity, addressToEntity(msg.sender));
+    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(buildEntity, block.timestamp);
+    DefenceComponent(getAddressById(components, DefenceComponentID)).set(buildEntity, defenceInitialAmount);
+    LevelComponent(getAddressById(components, LevelComponentID)).set(buildEntity, godownInitialLevel);
+    EntityTypeComponent(getAddressById(components, EntityTypeComponentID)).set(buildEntity, entity_type);
+    BalanceComponent(getAddressById(components, BalanceComponentID)).set(buildEntity, godownInitialBalance);
+    PopulationComponent(getAddressById(components, PopulationComponentID)).set(buildEntity, initialEntityPopulation);
+    FuelComponent(getAddressById(components, FuelComponentID)).set(buildEntity, baseInitialfuel);
   }
 
-//From UI we will pass the source ship yard, the destination where the new vessel should be built and the type of vessel we want to build
-  function executeTyped(
-    uint256 sourceEntity,
-    int32 x,
-    int32 y,
-    uint256 entity_type
-  ) public returns (bytes memory) {
-    return execute(abi.encode(sourceEntity, x, y, entity_type));
+  //Input parameters are Shipyard, Build location and what you want to build
+
+  function executeTyped(uint256 ShipyardEntity, int32 x, int32 y, uint256 entity_type) public returns (bytes memory) {
+    return execute(abi.encode(ShipyardEntity, x, y, entity_type));
   }
 }
