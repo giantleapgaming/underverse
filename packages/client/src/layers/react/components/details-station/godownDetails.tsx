fe8977e8058f23f20660e7e2e8a1cf6ba8f46f1a
twinkal <twinkal.work@outlook.com>
2023-02-26 20:12:45 +0530

    fix(client): new 256 UI for godown added

--- packages/client/src/layers/react/components/details-station/godownDetails.tsx
+++ packages/client/src/layers/react/components/details-station/godownDetails.tsx
@@ -77,7 +77,7 @@ export const GodownDetails = ({ layers }: { layers: Layers }) => {
                   </p>
                 </S.Weapon>
                 <S.Weapon>
-                  <img src="/build-stations/box.png" />
+                  <img src="/build-stations/crystal.png" width="20px" height="20px" />
                   <p>
                     {+balance}/{+level}
                   </p>
