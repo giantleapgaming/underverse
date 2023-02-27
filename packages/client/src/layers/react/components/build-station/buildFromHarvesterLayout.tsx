d6c7ff2141378c84620fae8a1a8399176f96604d
Pranav <pranavkp.me@outlook.com>
2023-02-27 11:46:00 +0530

    fix(client): fix the details in the ui

--- packages/client/src/layers/react/components/build-station/buildFromHarvesterLayout.tsx
+++ packages/client/src/layers/react/components/build-station/buildFromHarvesterLayout.tsx
@@ -4,53 +4,51 @@ import { Layers } from "../../../../types";
 import { Mapping } from "../../../../utils/mapping";
 
 export const BuildFromHarvesterLayout = ({ layers }: { layers: Layers }) => {
-    const {
-        network: {
-            components: { Faction },
-            world,
-            network: { connectedAddress },
-        },
-        phaser: {
-            sounds,
-            components: { ShowStationDetails },
-            localApi: { setBuild },
-            localIds: { stationDetailsEntityIndex },
-        },
-    } = layers;
-    const entityIndex = world.entities.indexOf(connectedAddress.get());
-    const factionNumber = getComponentValueStrict(Faction, entityIndex).value;
-    const selectedEntity = getComponentValue(ShowStationDetails, stationDetailsEntityIndex)?.entityId;
+  const {
+    network: {
+      components: { Faction },
+      world,
+      network: { connectedAddress },
+    },
+    phaser: {
+      sounds,
+      components: { ShowStationDetails },
+      localApi: { setBuild },
+      localIds: { stationDetailsEntityIndex },
+    },
+  } = layers;
+  const entityIndex = world.entities.indexOf(connectedAddress.get());
+  const selectedEntity = getComponentValue(ShowStationDetails, stationDetailsEntityIndex)?.entityId;
 
-    const build = (entityType: number) => {
-        sounds["click"].play();
-        setBuild({ x: 0, y: 0, canPlace: true, entityType, show: false, isBuilding: true });
-    };
+  const build = (entityType: number) => {
+    sounds["click"].play();
+    setBuild({ x: 0, y: 0, canPlace: true, entityType, show: false, isBuilding: true });
+  };
 
-    if (selectedEntity) {
-        return (
-            <S.Flex>
-                <S.Button onClick={() => build(Mapping.residential.id)} title="Habitat">
-                    <S.Img src="/layout/hex.png" width="70px" height="64px" />
-                    <S.Img src={`/build-stations/${+factionNumber}-1-0.png`} width="30px" height="30px" />
-                </S.Button>
-                <S.Button onClick={() => build(Mapping.godown.id)} title="Depot">
-                    <S.Img src="/layout/hex.png" width="70px" height="64px" />
-                    <S.Img src={`/build-stations/cargo-1-0.png`} width="30px" height="30px" />
-                </S.Button>
-                <S.Button onClick={() => build(Mapping.shipyard.id)} title="Shipyard">
-                    <S.Img src="/layout/hex.png" width="70px" height="64px" />
-                    <S.Img src={`/build-stations/shipyard-${+factionNumber}.png`} width="30px" height="30px" />
-                </S.Button>
-            </S.Flex>
-        );
-    }
-    else {
-        return null;
-    }
-}
+  if (selectedEntity) {
+    return (
+      <S.Flex>
+        <S.Button onClick={() => build(Mapping.residential.id)} title="Habitat">
+          <S.Img src="/layout/hex.png" width="70px" height="64px" />
+          <S.Img src={`/build-stations/space-station.png`} width="30px" height="30px" />
+        </S.Button>
+        <S.Button onClick={() => build(Mapping.godown.id)} title="Depot">
+          <S.Img src="/layout/hex.png" width="70px" height="64px" />
+          <S.Img src={`/build-stations/cargo.png`} width="30px" height="30px" />
+        </S.Button>
+        <S.Button onClick={() => build(Mapping.shipyard.id)} title="Shipyard">
+          <S.Img src="/layout/hex.png" width="70px" height="64px" />
+          <S.Img src={`/build-stations/shipyard.png`} width="30px" height="30px" />
+        </S.Button>
+      </S.Flex>
+    );
+  } else {
+    return null;
+  }
+};
 
 const S = {
-    Flex: styled.div`
+  Flex: styled.div`
     display: flex;
     flex-wrap: wrap;
     align-items: center;
@@ -61,7 +59,7 @@ const S = {
     justify-content: space-around;
     padding-bottom: 30px;
   `,
-    Button: styled.div`
+  Button: styled.div`
     cursor: pointer;
     position: relative;
     width: 54px;
@@ -71,14 +69,14 @@ const S = {
     justify-content: center;
     padding-top: 50px;
   `,
-    Img: styled.img`
+  Img: styled.img`
     display: flex;
     position: absolute;
     align-items: center;
     justify-content: center;
     margin: auto;
   `,
-    Text: styled.div`
+  Text: styled.div`
     align-items: center;
     font-size: 12px;
   `,
