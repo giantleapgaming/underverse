6583ca107957f247a6add53d54bb932b93439940
moreshk <moreshk@gmail.com>
2023-03-04 23:21:55 +1100

    feat(buy weapon): nft integration

--- packages/contracts/src/systems/BuyWeaponSystem.sol
+++ packages/contracts/src/systems/BuyWeaponSystem.sol
@@ -5,15 +5,17 @@ import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById, addressToEntity } from "solecs/utils.sol";
 import { CashComponent, ID as CashComponentID } from "../components/CashComponent.sol";
 import { PositionComponent, ID as PositionComponentID, Coord } from "../components/PositionComponent.sol";
-import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../components/LastUpdatedTimeComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "../components/OwnedByComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
 import { OffenceComponent, ID as OffenceComponentID } from "../components/OffenceComponent.sol";
 import { FactionComponent, ID as FactionComponentID } from "../components/FactionComponent.sol";
 import { getCurrentPosition, getPlayerCash, getFactionWeaponCosts } from "../utils.sol";
-import { MULTIPLIER, MULTIPLIER2, MISSILE_COST, Faction } from "../constants.sol";
+import { MULTIPLIER, MISSILE_COST, Faction } from "../constants.sol";
 import "../libraries/Math.sol";
 import { EntityTypeComponent, ID as EntityTypeComponentID } from "../components/EntityTypeComponent.sol";
+import { NFTIDComponent, ID as NFTIDComponentID } from "../components/NFTIDComponent.sol";
+import { checkNFT } from "../utils.sol";
+import { nftContract } from "../constants.sol";
 
 uint256 constant ID = uint256(keccak256("system.BuyWeapon"));
 
@@ -21,11 +23,15 @@ contract BuyWeaponSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function execute(bytes memory arguments) public returns (bytes memory) {
-    (uint256 godownEntity, uint256 buyQuantity) = abi.decode(arguments, (uint256, uint256));
+    (uint256 godownEntity, uint256 buyQuantity, uint256 nftID) = abi.decode(arguments, (uint256, uint256, uint256));
+
+    require(checkNFT(nftContract, nftID), "User wallet does not have the required NFT");
+
+    uint256 playerID = NFTIDComponent(getAddressById(components, NFTIDComponentID)).getEntitiesWithValue(nftID)[0];
+    require(playerID != 0, "NFT ID to Player ID mapping has to be 1:1");
 
     require(
-      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(godownEntity) ==
-        addressToEntity(msg.sender),
+      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(godownEntity) == playerID,
       "Godown not owned by user"
     );
 
@@ -50,14 +56,9 @@ contract BuyWeaponSystem is System {
       "Provided weapon quantity is more than available godown limit"
     );
 
-    uint256 playerCash = getPlayerCash(
-      CashComponent(getAddressById(components, CashComponentID)),
-      addressToEntity(msg.sender)
-    );
+    uint256 playerCash = getPlayerCash(CashComponent(getAddressById(components, CashComponentID)), playerID);
 
-    uint256 userFaction = FactionComponent(getAddressById(components, FactionComponentID)).getValue(
-      addressToEntity(msg.sender)
-    );
+    uint256 userFaction = FactionComponent(getAddressById(components, FactionComponentID)).getValue(playerID);
 
     uint256 factionCostPercent = getFactionWeaponCosts(Faction(userFaction));
 
@@ -67,24 +68,15 @@ contract BuyWeaponSystem is System {
     require(playerCash >= totalPrice, "Not enough money to buy such weapon quantity");
 
     // update player data
-    CashComponent(getAddressById(components, CashComponentID)).set(
-      addressToEntity(msg.sender),
-      playerCash - totalPrice
-    );
-    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(
-      addressToEntity(msg.sender),
-      block.timestamp
-    );
-
+    CashComponent(getAddressById(components, CashComponentID)).set(playerID, playerCash - totalPrice);
     // update godown data
     OffenceComponent(getAddressById(components, OffenceComponentID)).set(
       godownEntity,
       godownExisitingWeaponQuantity + buyQuantity
     );
-    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(godownEntity, block.timestamp);
   }
 
-  function executeTyped(uint256 godownEntity, uint256 buyQuantity) public returns (bytes memory) {
-    return execute(abi.encode(godownEntity, buyQuantity));
+  function executeTyped(uint256 godownEntity, uint256 buyQuantity, uint256 nftID) public returns (bytes memory) {
+    return execute(abi.encode(godownEntity, buyQuantity, nftID));
   }
 }
