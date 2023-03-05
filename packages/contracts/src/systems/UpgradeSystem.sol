2f55904baed58f158ef0f90d391f152c2af18066
moreshk <moreshk@gmail.com>
2023-03-05 14:12:31 +1100

    feat(upgrade): nft integration

--- packages/contracts/src/systems/UpgradeSystem.sol
+++ packages/contracts/src/systems/UpgradeSystem.sol
@@ -4,13 +4,16 @@ import "solecs/System.sol";
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById, addressToEntity } from "solecs/utils.sol";
 import { CashComponent, ID as CashComponentID } from "../components/CashComponent.sol";
-import { LastUpdatedTimeComponent, ID as LastUpdatedTimeComponentID } from "../components/LastUpdatedTimeComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "../components/OwnedByComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
 import { DefenceComponent, ID as DefenceComponentID } from "../components/DefenceComponent.sol";
 import { FactionComponent, ID as FactionComponentID } from "../components/FactionComponent.sol";
 import { getPlayerCash, getFactionUpgradeCosts } from "../utils.sol";
-import { actionDelayInSeconds, MULTIPLIER, Faction } from "../constants.sol";
+import { MULTIPLIER, Faction } from "../constants.sol";
+import { NFTIDComponent, ID as NFTIDComponentID } from "../components/NFTIDComponent.sol";
+import { EncounterComponent, ID as EncounterComponentID } from "../components/EncounterComponent.sol";
+import { checkNFT } from "../utils.sol";
+import { nftContract } from "../constants.sol";
 
 uint256 constant ID = uint256(keccak256("system.Upgrade"));
 
@@ -18,19 +21,16 @@ contract UpgradeSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function execute(bytes memory arguments) public returns (bytes memory) {
-    uint256 entity = abi.decode(arguments, (uint256));
+    (uint256 entity, uint256 nftID) = abi.decode(arguments, (uint256, uint256));
+
+    require(checkNFT(nftContract, nftID), "User wallet does not have the required NFT");
+
+    uint256 playerID = NFTIDComponent(getAddressById(components, NFTIDComponentID)).getEntitiesWithValue(nftID)[0];
+    require(playerID != 0, "NFT ID to Player ID mapping has to be 1:1");
 
     require(
-      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(entity) == addressToEntity(msg.sender),
+      OwnedByComponent(getAddressById(components, OwnedByComponentID)).getValue(entity) == playerID,
       "Entity not owned by user"
-    );
-
-    uint256 playerLastUpdatedTime = LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID))
-      .getValue(addressToEntity(msg.sender));
-
-    require(
-      playerLastUpdatedTime > 0 && block.timestamp >= playerLastUpdatedTime + actionDelayInSeconds,
-      "Need 0 seconds of delay between actions"
     );
 
     uint256 selectedEntityLevel = LevelComponent(getAddressById(components, LevelComponentID)).getValue(entity);
@@ -39,39 +39,26 @@ contract UpgradeSystem is System {
 
     require(selectedEntityLevel < 8, "Maximum level reached");
 
-    uint256 userFaction = FactionComponent(getAddressById(components, FactionComponentID)).getValue(
-      addressToEntity(msg.sender)
-    );
+    uint256 userFaction = FactionComponent(getAddressById(components, FactionComponentID)).getValue(playerID);
 
     uint256 factionCostPercent = getFactionUpgradeCosts(Faction(userFaction));
 
     uint256 nextLevel = selectedEntityLevel + 1;
 
-    uint256 upgradeCost = ((nextLevel**2) * 1000 * MULTIPLIER * factionCostPercent) / 100;
+    uint256 upgradeCost = ((nextLevel ** 2) * 1000 * MULTIPLIER * factionCostPercent) / 100;
 
-    uint256 playerCash = getPlayerCash(
-      CashComponent(getAddressById(components, CashComponentID)),
-      addressToEntity(msg.sender)
-    );
+    uint256 playerCash = getPlayerCash(CashComponent(getAddressById(components, CashComponentID)), playerID);
 
     require(playerCash >= upgradeCost, "Not enough money to upgrade");
 
     uint256 selectedEntityDefence = DefenceComponent(getAddressById(components, DefenceComponentID)).getValue(entity);
 
-    CashComponent(getAddressById(components, CashComponentID)).set(
-      addressToEntity(msg.sender),
-      playerCash - upgradeCost
-    );
+    CashComponent(getAddressById(components, CashComponentID)).set(playerID, playerCash - upgradeCost);
     LevelComponent(getAddressById(components, LevelComponentID)).set(entity, nextLevel);
     DefenceComponent(getAddressById(components, DefenceComponentID)).set(entity, selectedEntityDefence + 100);
-    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(
-      addressToEntity(msg.sender),
-      block.timestamp
-    );
-    LastUpdatedTimeComponent(getAddressById(components, LastUpdatedTimeComponentID)).set(entity, block.timestamp);
   }
 
-  function executeTyped(uint256 entity) public returns (bytes memory) {
-    return execute(abi.encode(entity));
+  function executeTyped(uint256 entity, uint256 nftID) public returns (bytes memory) {
+    return execute(abi.encode(entity, nftID));
   }
 }
