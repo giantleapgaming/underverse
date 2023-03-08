0dabfaeda410030001e2d8f406d6e122d932bc0a
twinkal <twinkal.work@outlook.com>
2023-03-08 23:19:31 +0530

    fix(client): only 2 orbits are showing, 15th and 100th

--- packages/client/src/layers/phaser/system/createMapSystem.ts
+++ packages/client/src/layers/phaser/system/createMapSystem.ts
@@ -16,18 +16,20 @@ export function createMapSystem(network: NetworkLayer, phaser: PhaserLayer) {
   } = phaser;
   for (let i = 1; i < 21; i++) {
     const circle = phaserScene.add.circle(tileWidth / 2, tileWidth / 2);
-    const label = phaserScene.add.text(tileWidth / 2, -(i * 5 * tileWidth - tileWidth / 2), `${i * 5}`, {
-      fontSize: "100px",
-      color: "#c0c0c0",
-    });
+    // const label = phaserScene.add.text(tileWidth / 2, -(i * 5 * tileWidth - tileWidth / 2), `${i * 5}`, {
+    //   fontSize: "100px",
+    //   color: "#c0c0c0",
+    // });
     if (i < 3) {
-      circle.setStrokeStyle(0.4, 0x2d2d36);
+      circle.setStrokeStyle(0, 0x2d2d36);
     } else if (i === 3) {
       circle.setStrokeStyle(3, 0x2d2d36);
-    } else circle.setStrokeStyle(0.08, 0x2d2d36);
+    } else if (i === 20) {
+      circle.setStrokeStyle(2, 0x2d2d36);
+    } else circle.setStrokeStyle(0, 0x2d2d36);
     circle.setDisplaySize(tileHeight * 10 * i, tileHeight * 10 * i);
-    label.setOrigin(0.5, 0.5);
-    label.setDepth(20);
+    // label.setOrigin(0.5, 0.5);
+    // label.setDepth(20);
   }
 
   input.pointermove$.subscribe(({ pointer }) => {
