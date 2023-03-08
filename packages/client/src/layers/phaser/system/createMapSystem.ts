edbf186183bc9164186c080f326623497fd7aca1
twinkal <twinkal.work@outlook.com>
2023-03-08 10:51:09 +0530

    feat: 100 orbits are added

--- packages/client/src/layers/phaser/system/createMapSystem.ts
+++ packages/client/src/layers/phaser/system/createMapSystem.ts
@@ -14,15 +14,23 @@ export function createMapSystem(network: NetworkLayer, phaser: PhaserLayer) {
       },
     },
   } = phaser;
-  for (let i = 1; i < 11; i++) {
+  for (let i = 1; i < 101; i++) {
     const circle = phaserScene.add.circle(tileWidth / 2, tileWidth / 2);
     const label = phaserScene.add.text(tileWidth / 2, -(i * 5 * tileWidth - tileWidth / 2), `${i * 5}`, {
       fontSize: "100px",
       color: "#c0c0c0",
     });
-    if (i == 3) {
+    if (i < 3) {
+      circle.setStrokeStyle(0.4, 0x2d2d36);
+    } else if (i === 3) {
       circle.setStrokeStyle(3, 0x2d2d36);
-    } else circle.setStrokeStyle(0.3, 0x2d2d36);
+    } else if (i > 3 && i <= 25) {
+      circle.setStrokeStyle(0.1, 0x2d2d36);
+    } else if (i > 25 && i <= 50) {
+      circle.setStrokeStyle(0.05, 0x2d2d36);
+    } else if (i > 50 && i <= 75) {
+      circle.setStrokeStyle(0.025, 0x2d2d36);
+    } else circle.setStrokeStyle(0.01, 0x2d2d36);
     circle.setDisplaySize(tileHeight * 10 * i, tileHeight * 10 * i);
     label.setOrigin(0.5, 0.5);
     label.setDepth(20);
