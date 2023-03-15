52a2f98422ce8e6e5dba3a9bb6c5673a8b4e5bab
Pranav <pranavkp.me@outlook.com>
2023-03-15 09:45:30 +0530

    fix(client): fix the cost showing on the build harvester

--- packages/client/src/layers/network/systems/build/harvester.ts
+++ packages/client/src/layers/network/systems/build/harvester.ts
@@ -1,10 +1,18 @@
 import { Sprites } from "./../../../phaser/constants";
 import { tileCoordToPixelCoord } from "@latticexyz/phaserx";
-import { defineComponentSystem, getComponentValue } from "@latticexyz/recs";
+import {
+  defineComponentSystem,
+  getComponentEntities,
+  getComponentValue,
+  getComponentValueStrict,
+} from "@latticexyz/recs";
 import { NetworkLayer } from "../../../network";
 import { PhaserLayer } from "../../../phaser";
 import { Mapping } from "../../../../utils/mapping";
 import { generateColorsFromWalletAddress } from "../../../../utils/hexToColour";
+import { getNftId } from "../../utils/getNftId";
+import { factionData } from "../../../../utils/constants";
+import { convertPrice } from "../../../react/utils/priceConverter";
 
 export function buildHarvesterSystem(network: NetworkLayer, phaser: PhaserLayer) {
   const {
@@ -24,7 +32,7 @@ export function buildHarvesterSystem(network: NetworkLayer, phaser: PhaserLayer)
 
   const {
     network: { connectedAddress },
-    components: { Position },
+    components: { Position, Faction, NFTID },
   } = network;
 
   defineComponentSystem(world, Build, () => {
@@ -82,27 +90,40 @@ export function buildHarvesterSystem(network: NetworkLayer, phaser: PhaserLayer)
         },
       });
       const textPosition = tileCoordToPixelCoord({ x: xCoord, y: yCoord }, tileWidth, tileHeight);
+      const nftDetails = getNftId({ network, phaser });
+      const ownedByIndex = [...getComponentEntities(NFTID)].find((nftId) => {
+        const nftIdValue = getComponentValueStrict(NFTID, nftId)?.value;
+        return nftIdValue && +nftIdValue === nftDetails?.tokenId;
+      });
+      const faction = getComponentValue(Faction, ownedByIndex)?.value;
+      const price = faction ? factionData[+faction]?.build * 50000 : 0;
       textWhite.setComponent({
         id: "build-harvester-station-text-white",
         once: (gameObject) => {
           gameObject.setPosition(textPosition.x + 40, textPosition.y - 35);
           gameObject.depth = 4;
-          gameObject.setText(`2`);
-          gameObject.setFontSize(16);
+          gameObject.setText(
+            typeof stationDetails === "undefined" && buildDetails.entityType == Mapping.harvester.id
+              ? convertPrice(price)
+              : `2`
+          );
+          gameObject.setFontSize(100);
           gameObject.setFontStyle("bold");
           gameObject.setColor("#ffffff");
         },
       });
-      const mineral = objectPool.get("build-harvester-station-text-white-m", "Sprite");
-      mineral.setComponent({
-        id: "build-harvester-station-text-white-m",
-        once: (gameObject) => {
-          gameObject.setPosition(textPosition.x + 10, textPosition.y - 30);
-          gameObject.setTexture(HoverSprite.assetKey, `mineral.png`);
-          gameObject.depth = 4;
-          gameObject.setOrigin(0.5, 0.5);
-        },
-      });
+      if (!(typeof stationDetails === "undefined" && buildDetails.entityType == Mapping.harvester.id)) {
+        const mineral = objectPool.get("build-harvester-station-text-white-m", "Sprite");
+        mineral.setComponent({
+          id: "build-harvester-station-text-white-m",
+          once: (gameObject) => {
+            gameObject.setPosition(textPosition.x + 10, textPosition.y - 30);
+            gameObject.setTexture(HoverSprite.assetKey, `mineral.png`);
+            gameObject.depth = 4;
+            gameObject.setOrigin(0.5, 0.5);
+          },
+        });
+      }
     } else if (distanceFromCenter > 15) {
       const selectedStationPosition = getComponentValue(Position, stationDetails);
       if (selectedStationPosition) {
