af08b88d7e8c23e7418c05ffdec7e7570c90f57e
twinkal <twinkal.work@outlook.com>
2023-04-18 08:48:08 +0530

    fix(tutorial client): twitter share message fixed

--- packages/client/src/layers/react/components/Congratulations.tsx
+++ packages/client/src/layers/react/components/Congratulations.tsx
@@ -185,9 +185,9 @@ const Congratulations = ({ layers }: { layers: Layers }) => {
                     onClick={() => {
                       const text =
                         (isCadet &&
-                          `I've just earned my Cadet Wings NFT for The Underverse MMORTS [@giantleapgg](https://twitter.com/giantleapgg)! \n\nThink you've got what it takes? \n\nGet one for yourself here and join me in the Underverse - https://tutorial.giantleap.gg/ \n\n #freenft #web3games`) ||
+                          `I've just earned my Cadet Wings NFT for The Underverse MMORTS @giantleapgg! \n\nThink you've got what it takes? \n\nGet one for yourself here and join me in the Underverse - https://tutorial.giantleap.gg/ \n\n #freenft #web3games #underverse`) ||
                         (isRookie &&
-                          `I've just earned my Rookie Wings NFT for The Underverse MMORTS [@giantleapgg](https://twitter.com/giantleapgg)! \n\nThink you've got what it takes? \n\nGet one for yourself here and join me in the Underverse - https://tutorial.giantleap.gg/ \n\n #freenft #web3games`);
+                          `I've just earned my Rookie Wings NFT for The Underverse MMORTS @giantleapgg! \n\nThink you've got what it takes? \n\nGet one for yourself here and join me in the Underverse - https://tutorial.giantleap.gg/ \n\n #freenft #web3games #underverse`);
                       const twitterShareURL = "https://twitter.com/share?" + "text=" + encodeURIComponent(text || "");
                       console.log(twitterShareURL);
                       const popupWidth = 550;
